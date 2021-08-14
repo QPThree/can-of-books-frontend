9157d289ef8b20ee7ddb46b28058b213b6c9f9a6
QPThree <youngqp3@gmail.com>
2021-08-14 14:33:22 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-y7gnygk3.us.auth0.com"
+    clientId="kPb9274X2vvh99j39wKIETARLFjh8EGA"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
